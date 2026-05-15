ca2befb302e7815ccd3a217789e283a937a42947
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-15 15:29:01 +0200

    Update joueur.c

--- joueur.c
+++ joueur.c
@@ -9,25 +9,26 @@ int constructJoueur (defJoueur joueur[3]){
      scanf("%d", &n);
      getchar();
      if (n!=2 && n!=3 && n!=99) {
-       printf("Erreur :  le nombre de joueur est incorrect\n");}
-  } while (n!=2 && n!=3 && n!=99);
+       printf("Erreur :  le nombre de joueur est incorrect\n");
+      }
+   } while (n!=2 && n!=3 && n!=99);
   if (n==99) {
-   return(n);
-  }
-  for(int i=0; i<n; i++) {
-   joueur[i].numJoueur = i+1;
-   joueur[i].pieceJoueur = i+1;
-   joueur[i].aGagner = 0;
-   printf("Entrez le nom du joueur %d et tapez sur la touche Entrée \n", i+1);
-   carlu = ' ';
-   j=0;
-   while (carlu !='\n') {
-     scanf("%c", &carlu);
-     if (j < 10) {
-       joueur[i].nomJoueur[j] = carlu;
-       j++;
-     }
+    return(n);
    }
-  }
+  for(int i=0; i<n; i++) {
+    joueur[i].numJoueur = i+1;
+    joueur[i].pieceJoueur = i+1;
+    joueur[i].aGagner = 0;
+    printf("Entrez le nom du joueur %d et tapez sur la touche Entrée \n", i+1);
+    carlu = ' ';
+    j=0;
+    while (carlu !='\n') {
+      scanf("%c", &carlu);
+      if (j < 10) {
+        joueur[i].nomJoueur[j] = carlu;
+        j++;
+      }
+    }
+   }
   return(n);
  }
