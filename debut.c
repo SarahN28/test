bbbc65b3abfad1a6563e47e3a1264d73cbfc8146
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-15 14:48:15 +0200

    Update debut.c

--- debut.c
+++ debut.c
@@ -15,3 +15,16 @@ typedef struct {
   char pieceJoueur;
   int aGagner;
 } defJoueur;
+
+// initialisation de la grille de jeu
+ void createPiste(int tab[][NBCOL], int nbl, int nbc){
+  for (int i=0; i<nbl; i++){
+    for(int j=0; j<nbc; j++) {
+      tab[i][j] = 0;
+    }
+  }
+  tab[0][0]=1;                //carré sur L1 C1
+  tab[0][nbc-1]=1;                //carré sur L1 C8
+  tab[nbl-1][0]=1;                //carré sur L6 C1
+  tab[nbl-1][nbc-1]=1;                //carré sur L6 C8
+}
