8ad020c3821d51ea56bae2adc13b35895fafe6aa
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-15 18:25:02 +0200

    Update joueur.c

--- joueur.c
+++ joueur.c
@@ -4,31 +4,32 @@ int constructJoueur (defJoueur joueur[3]){
   int j;
   char carlu;
   do{
-     printf("Nombre de joueurs:\n");
-     printf("2 : 2joueurs       3 : 3joueurs       sinon pour sortir tapez 99 :\n");
+     printf("Entrez le nombre de joueurs : 2 ou 3, pour sortir tapez 99 :\n");
      scanf("%d", &n);
-     getchar();
+     carlu = getchar();
+     while (carlu !='\n') {
+      carlu = getchar();
+     }
      if (n!=2 && n!=3 && n!=99) {
-       printf("Erreur :  le nombre de joueur est incorrect\n");
-      }
-   } while (n!=2 && n!=3 && n!=99);
+       printf("Erreur :  le nombre de joueur est incorrect\n");}
+  } while (n!=2 && n!=3 && n!=99);
   if (n==99) {
-    return n;
-   }
+   return n;
+  }
   for(int i=0; i<n; i++) {
-    joueur[i].numJoueur = i+1;
-    joueur[i].pieceJoueur = i+2;
-    joueur[i].aGagner = 0;
-    printf("Entrez le nom du joueur %d et tapez sur la touche Entrée \n", i+1);
-    carlu = ' ';
-    j=0;
-    while (carlu !='\n') {
-      scanf("%c", &carlu);
-      if (j < 10) {                                    // Le pseudo est au max de 10 caractères
-        joueur[i].nomJoueur[j] = carlu;
-        j++;
-      }
-    }
+   joueur[i].numJoueur = i+1;
+   joueur[i].pieceJoueur = i+2;
+   joueur[i].aGagner = 0;
+   printf("Entrez le nom du joueur %d et tapez sur la touche Entrée \n", i+1);
+   carlu = ' ';
+   j=0;
+   while (carlu !='\n') {
+     scanf("%c", &carlu);
+     if (j < 10) {
+       joueur[i].nomJoueur[j] = carlu;
+       j++;
+     }
    }
+  }
   return n;
  }
