cc3a750e7e22be557e2eb37e8265bec3e0eb1dc6
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-15 18:30:24 +0200

    Update piece et gravité.c

--- piece et gravité.c
+++ piece et gravité.c
@@ -20,14 +20,15 @@
  
  // insertion  valeur  en colonne c numcol
  // vaut 2 si premier joueur, 3 si second joueur 
-int deplacement_bas(int (*tab)[NBCOL], int nbJ, int numcol){
-  int i;
-  i=0;
-  while(tab[i][numcol]==0 && i<6){
+int deplacement_bas(int tab[][NBCOL], int nbJ, int numcol){
+  int i = 0;
+  while (tab[i][numcol]==0 && i<6){
     i = i+1;
   }
   // i contient la première ligne non vide (arrêt car !=0)
-  if (i==0) { return(-1);} // la colonne est pleine : insertion du jeton impossible
+  if (i==0) {
+    return(-1);
+   } // la colonne est pleine : insertion du jeton impossible
   tab[i-1][numcol] = nbJ;
-  return(i-1);
+  return (i-1);
  }
