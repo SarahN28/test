8fe39abf58f9ff0ff214dfe8ab49cd78db5764b0
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-16 15:46:11 +0200

    Update choicePiv.c

--- choicePiv.c
+++ choicePiv.c
@@ -18,48 +18,45 @@ pivot choixPivot(int n, int lig, int col) {              //lig = (i+1) of previo
   // le carré doit être dans grille : donc le pivot - n case, et picot + n cases doit être dans la grille
   if ((ligmin-n) < 0) {
     ligmin = n;
-  }
+   }
   if ((ligmax+n) > 5 ) {
     ligmax = 5 - n;
-  }
-  
-   if ((colmin-n) < 0) {
+   }  
+  if ((colmin-n) < 0) {
     colmin = n;
-  }
+   }
   if ((colmax + n) > 7 ) {
     colmax = 7-n;
-  }
+   }
    
   // la ligne affichée est à +1 de l'indice
-    while (j<(colmin+1) || (j>(colmax+1) && j!=99)){
-      printf("Choisir la colonne du pivot : comprise entre %d et %d ou 99 pour sortir du jeu\n", colmin+1, colmax+1);
-      scanf("%d", &j);
+  while (j<(colmin+1) || (j>(colmax+1) && j!=99)){
+    printf("Choisir la colonne du pivot : comprise entre %d et %d ou 99 pour sortir du jeu\n", colmin+1, colmax+1);
+    scanf("%d", &j);
+    carlu = getchar();
+    while (carlu != '\n') {
+      carlu = getchar();
+     }
+   }
+     
+  if (j!=99) {
+    while (i<(ligmin +1) || i>(ligmax+1)){ 
+      printf("Choisir la ligne du pivot : comprise entre %d et %d \n", ligmin+1, ligmax+1);
+      scanf("%d", &i);
       carlu = getchar();
       while (carlu != '\n') {
-       carlu = getchar();
-      }
-     }
-     
-    if (j!=99) {
-      while (i<(ligmin +1) || i>(ligmax+1)) {
-        printf("Choisir la ligne du pivot : comprise entre %d et %d \n", ligmin+1, ligmax+1);
-        scanf("%d", &i);
-       carlu = getchar();
-       while (carlu != '\n') {
         carlu = getchar();
        }
-      }
-     printf("Votre pivot est en l%d, c%d \n", i, j);
-     i--;
-     j--;
-     
-    }
-    else {
+     }
+    printf("Votre pivot est en l%d, c%d \n", i, j);
+    i--;
+    j--; 
+   }
+   else {
      i = 99;
     }
-   
-  c.ligne = i;
-  c.colonne = j;
-  return c;
+  
+  c.ligne = i;                           //line piv
+  c.colonne = j;                         //col piv
+  return c;                              //return piv
  }
- 
