59777a89b7292af270eb3cd36d7777cdabcb28f4
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-16 16:21:49 +0200

    Update piece et gravité.c

--- piece et gravité.c
+++ piece et gravité.c
@@ -2,7 +2,7 @@
  int saisiecolonne() {
    int n=0;
    char carlu;
-    do{
+   do{
      printf("Entrez le numéro de colonne pour insérer votre jeton, ou 99 pour sortir : \n");
      scanf("%d", &n);
      carlu = getchar();
@@ -10,26 +10,29 @@
        carlu = getchar();
      }
      if (n<1 || (n>8 && n!=99)) {
-       printf("Erreur :  le numéro de colonne est incorrecte\n");}
-  } while (n<1 || (n>8 && n!=99));
+       printf("Erreur :  le numéro de colonne est incorrecte\n");
+     }
+   } while (n<1 || (n>8 && n!=99));
   if (n!=99) {
-    return(n-1);}
-  else {return(n);
+    return (n-1);
+  }
+  else {
+   return (n);
   } 
  }
  
-
  // insertion  valeur  en colonne c numcol
- // vaut 2 si premier joueur, 3 si second joueur 
+ // vaut 2 si premier joueur, 3 si second joueur, 4 si troisieme joueur
 int deplacement_bas(int tab[NBLIG][NBCOL], int nbJ, int numcol){
-  int i;
-  i=0;
+  int i=0;
   while(tab[i][numcol]==0 && i<6){
     i = i+1;
   }
   // i contient la première ligne non vide (arrêt car !=0)
-  if (i==0) { return(-1);} // la colonne est pleine : insertion du jeton impossible
+  if (i==0) {
+    return (-1);
+   } // la colonne est pleine : insertion du jeton impossible
   tab[i-1][numcol] = nbJ;
-  return(i-1);
+  return (i-1);
  }
 
