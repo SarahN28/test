70f35c51606e51af08d453cdc224ac8633ee77f9
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-16 14:49:42 +0200

    Update debut.c

--- debut.c
+++ debut.c
@@ -17,3 +17,37 @@
   tab[nbl-1][0]=1;                //carré sur L6 C1
   tab[nbl-1][nbc-1]=1;                //carré sur L6 C8
 }
+
+// affichage de la grille de jeu
+void printGame(int tab[][NBCOL], int nbl, int nbc){
+  int k;
+  printf("-\n");
+  printf("    1  2  3  4  5  6  7  8\n");
+  for (int i=0; i<=5; i++){ 
+    printf ("%d ", i+1);
+    for (int j=0; j<nbc; j++){
+      if(tab[i][j]==0){
+        printf("|    ");
+       }
+      else if(tab[i][j]==1){
+        printf("|####");
+       }
+      else if(tab[i][j]==2){
+        printf("| )( ");
+       }
+      else if(tab[i][j]==3){
+        printf("| >< ");
+       }
+      else if(tab[i][j]==4){
+        printf("| ¤¤ ");
+       }
+     }
+    printf("| \n");
+    printf("  ");
+    for (k = 0; k<nbc; k++) {
+    printf("-----");
+    }  
+    printf("-\n");
+   }
+ }
+ 
