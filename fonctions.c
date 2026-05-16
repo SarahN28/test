02420059b6d49d80b13dff457cb578116eb022f0
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-16 15:02:43 +0200

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -87,4 +87,18 @@ void printGame(int tab[][NBCOL], int nbl, int nbc){
     printf("-\n");
    }
  }
+
  
+// sélection du pivot
+int sizePivot(){
+  int piv;
+  piv = rand()%2;
+  if(piv==0){
+    piv = 3;
+  }
+  else{
+  piv = 5;
+  }
+printf("la largeur du carré pivotant est de %d cases \n", piv);
+return piv;
+}
