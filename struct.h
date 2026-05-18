1df67a951a2c8a15dec18979c0f8ca6a6709c8f9
SarahN28 <sarah.ngo@etu.cyu.fr>
2026-05-18 15:09:08 +0200

    Update struct.h

--- struct.h
+++ struct.h
@@ -12,7 +12,7 @@ typedef struct {
 // les données du jeu : sont mises dans une structure pour pourvoir sauvegarder dans un fichier binaire, et les récupérer facilement
 typedef struct {
   int nbJoueur;
-  int numeroJoueur;
+  int numeroJoueur;                           //prochain joueur à jouer
   defJoueur jo[3];
   int grille[6][8];
 } jeu;
